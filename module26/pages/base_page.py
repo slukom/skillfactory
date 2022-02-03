b8e9dd418143c3bac3e8de3a31339745e93aa143
slukom <github.LILIA.9887>
2022-02-04 00:47:55 +0300

    module26

---
+++ module26/pages/base_page.py
@@ -0,0 +1,14 @@
+from urllib.parse import urlparse
+
+class BasePage(object):
+    # конструктор класса - специальный метод с ключевым словом __init__
+    # Нам нужны объект веб-драйвера, адрес страницы и время ожидани элементов
+    def __init__(self, driver, url, timeout=10):
+        self.driver = driver
+        self.url = url
+        self.driver.implicitly_wait(timeout) # задаёт максимальное время ожидания в секундах, в течении которого WebDriver пытается найти элемент, если он недоступен сразу же
+
+
+    def get_relative_link(self):
+        url = urlparse(self.driver.current_url)
+        return url.path
